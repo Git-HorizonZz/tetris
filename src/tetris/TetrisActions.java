0794cc8af12e5bf4578954d4f05a12b93145d621
BrandonWerbel <56608573+BrandonWerbel@users.noreply.github.com>
2022-01-24 12:19:30 -0600

    Reorg Actions

---
+++ src/tetris/TetrisActions.java
@@ -0,0 +1,52 @@
+package tetris;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class TetrisActions implements KeyListener {
+	
+	final Tetris game;
+	
+	public TetrisActions(Tetris game) {
+		this.game = game;
+	}
+	
+	public void keyTyped(KeyEvent e) { }
+		
+	public void keyPressed(KeyEvent e) 
+	{
+		switch (e.getKeyCode())
+		{
+			case KeyEvent.VK_UP:
+				game.rotate(-1);
+				break;
+			case KeyEvent.VK_DOWN:
+				game.rotate(+1);
+				break;
+			case KeyEvent.VK_LEFT:
+				game.move(-1);
+				break;
+			case KeyEvent.VK_RIGHT:
+				game.move(+1);
+				break;
+			case KeyEvent.VK_W:
+				game.rotate(-1);
+				break;
+			case KeyEvent.VK_S:
+				game.rotate(+1);
+				break;
+			case KeyEvent.VK_A:
+				game.move(-1);
+				break;
+			case KeyEvent.VK_D:
+				game.move(+1);
+				break;
+			case KeyEvent.VK_SPACE:
+				game.drop();
+				break;
+		} 
+	}
+	
+	public void keyReleased(KeyEvent e) { }
+	
+}
--- src/tetris/TetrisDriver.java
+++ src/tetris/TetrisDriver.java
@@ -26,46 +26,7 @@ public class TetrisDriver
 		f.setVisible(true);
 		f.setResizable(false);
 		
-		f.addKeyListener(new KeyListener() 
-		{
-			public void keyTyped(KeyEvent e) { }
-			
-			public void keyPressed(KeyEvent e) 
-			{
-				switch (e.getKeyCode())
-				{
-					case KeyEvent.VK_UP:
-						game.rotate(-1);
-						break;
-					case KeyEvent.VK_DOWN:
-						game.rotate(+1);
-						break;
-					case KeyEvent.VK_LEFT:
-						game.move(-1);
-						break;
-					case KeyEvent.VK_RIGHT:
-						game.move(+1);
-						break;
-					case KeyEvent.VK_W:
-						game.rotate(-1);
-						break;
-					case KeyEvent.VK_S:
-						game.rotate(+1);
-						break;
-					case KeyEvent.VK_A:
-						game.move(-1);
-						break;
-					case KeyEvent.VK_D:
-						game.move(+1);
-						break;
-					case KeyEvent.VK_SPACE:
-						game.drop();
-						break;
-				} 
-			}
-			
-			public void keyReleased(KeyEvent e) { }
-		});
+		f.addKeyListener(new TetrisActions(game));
 		
 		new Thread() 
 		{
