1a8a8882c1a1f5a3c2d2b6df4519d13913f08e8b
BrandonWerbel <brandon@werbel.org>
2022-01-26 17:43:57 -0500

    remove bin files

--- src/ai/aiDriver.java
+++ src/ai/aiDriver.java
@@ -1,18 +1,19 @@
 package ai;
+
 import java.awt.event.KeyEvent;
+import java.awt.Robot;
+import java.awt.Component;
 
 import tetris.*;
 
 import org.tensorflow.*;
 
 public class aiDriver {
-    
-    Tetris game = new Tetris();
 
     public static void main(String[] args) {
         TetrisDriver.main(args);
         for(int i = 0; i < 5; i++){
-            TetrisDriver.getActionsObject().keyPressed();
+            TetrisDriver.getActionsObject().keyPressed(game);
         }
     }
 }
